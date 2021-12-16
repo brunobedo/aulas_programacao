faa148069d31a3a1e3b0b3ccf6fda6188fa15a66
Bruno Bedo <bruno.bedo@usp.br>
2021-12-16 19:18:32 -0300

    Inseri o código do primeiro encontro

---
+++ encontro1/code_encontro_1.m
@@ -0,0 +1,10 @@
+% Encontro 1 
+% Objetivo: Conhecer o ambiente
+
+%%%%%%%%%%%%%%%%%%%% Início %%%%%%%%%%%%%%%%%%%%
+
+nome = input('What is your name?: ');
+
+disp([' O nome do participante é ',nome])
+
+%%%%%%%%%%%%%%%%%%%%%% Fim %%%%%%%%%%%%%%%%%%%%%
